80949ec74ddd0ea78e21755d9e1c78875ac48934
MaxCherya <maxchergik@gmail.com>
2025-05-13 20:19:02 +0300

    users added to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_2fa_enabled', 'last_login')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'is_2fa_enabled')
+    search_fields = ('username', 'email')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {
+            'fields': ('last_viewed', 'is_2fa_enabled', 'totp_secret'),
+        }),
+    )
